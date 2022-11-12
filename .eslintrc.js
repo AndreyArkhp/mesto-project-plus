c0df8775610cc2deea188652fe25e5b226395d38
Andrey Arkhipov <arhipov11@yandex.ru>
2022-11-12 16:09:52 +0300

    My first commit

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.ts', '.js', '.json'],
+      },
+    },
+    'import/extensions': ['.js', '.ts'],
+  },
+  rules: {
+    'no-console': 'off',
+    'linebreak-style': ['error', 'windows'],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        ts: 'never',
+      },
+    ],
+    'no-underscore-dangle': ['error', { allow: ['_id'] }],
+  },
+};
